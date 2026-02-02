dd9ddc72312d10c41fe33823cc061402f5945965
Ethan Sifferman <ethan@sifferman.dev>
2026-02-02 15:06:13 -0800

    added more indentation

--- test_regress/t/t_timing_nba_3.v
+++ test_regress/t/t_timing_nba_3.v
@@ -6,39 +6,39 @@
 
 module t;
 
-int delay = 0; always #1 delay = int'($time) / 4;
-task automatic do_delay;
-  if (delay > 0) #(delay);
-endtask
+  int delay = 0; always #1 delay = int'($time) / 4;
+  task automatic do_delay;
+    if (delay > 0) #(delay);
+  endtask
 
-// `a` should match `b`
-logic a = 0;
-always begin
-  a <= 1;
-  do_delay();
-  a <= 0;
+  // `a` should match `b`
+  logic a = 0;
+  always begin
+    a <= 1;
+    do_delay();
+    a <= 0;
 
-  #1;
-end
+    #1;
+  end
 
-logic b = 0;
-always begin
-  b <= 1;
-  do_delay();
-  b <= 0;
+  logic b = 0;
+  always begin
+    b <= 1;
+    do_delay();
+    b <= 0;
 
-  #1;
-  b <= 1; // this line should do nothing
-end
+    #1;
+    b <= 1; // this line should do nothing
+  end
 
-always #1 assert (a == b);
+  always #1 assert (a == b);
 
-initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars;
-  #20;
-  $write("*-* All Finished *-*\n");
-  $finish;
-end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    #20;
+    $write("*-* All Finished *-*\n");
+    $finish;
+  end
 
 endmodule
