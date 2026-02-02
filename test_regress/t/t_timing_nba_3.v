034ba0bd3affcb935f9c0aa668c9e399d175e371
Ethan Sifferman <ethan@sifferman.dev>
2026-02-02 13:14:37 -0800

    fixed indentation

--- test_regress/t/t_timing_nba_3.v
+++ test_regress/t/t_timing_nba_3.v
@@ -8,37 +8,37 @@ module t;
 
 int delay = 0; always #1 delay = int'($time) / 4;
 task automatic do_delay;
-    if (delay > 0) #(delay);
+  if (delay > 0) #(delay);
 endtask
 
 // `a` should match `b`
 logic a = 0;
 always begin
-    a <= 1;
-    do_delay();
-    a <= 0;
+  a <= 1;
+  do_delay();
+  a <= 0;
 
-    #1;
+  #1;
 end
 
 logic b = 0;
 always begin
-    b <= 1;
-    do_delay();
-    b <= 0;
+  b <= 1;
+  do_delay();
+  b <= 0;
 
-    #1;
-    b <= 1; // this line should do nothing
+  #1;
+  b <= 1; // this line should do nothing
 end
 
 always #1 assert (a == b);
 
 initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
-    #20;
-    $write("*-* All Finished *-*\n");
-    $finish;
+  $dumpfile("dump.vcd");
+  $dumpvars;
+  #20;
+  $write("*-* All Finished *-*\n");
+  $finish;
 end
 
 endmodule
